9e5a3793122a7f79760d6b2b75d6ce62f16a0625
DARKxAZANUR <azannur036@gmail.com>
2025-12-23 16:22:30 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+API_ID = 34988181              # apna api_id
+API_HASH = "2e3e05835b2aaac5d44b40ee3fa0cd7d"    # apna api_hash
+BOT_TOKEN = "8204558757:AAEgaVR32j15dr59PTUwZGd3BSHUdIhLi2w"  # botfather token
+YT_API_KEY = "AIzaSyCdcCSNiAzsyqvVmu3yNFD_wEXM5h2ptAw"  # optional (sirf search ke liye)
